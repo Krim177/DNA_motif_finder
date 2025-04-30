7949bc115c8a16f71466d1d99c53fd2e550a7390
Krimika Keemtee <61327579+Krim177@users.noreply.github.com>
2025-04-30 00:35:53 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+def find_motif_positions(sequence: str, motif: str) -> list:
+    """
+    Returns a list of start indices where `motif` occurs in `sequence`.
+    """
+    # your algorithm here (start with naive search!)
+    return []
